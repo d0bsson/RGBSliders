cf2f0879e7c80264ab121788e456807dc20bd186
David <ber.dev@yandex.ru>
2021-03-27 20:29:41 +0300

    update code. Change color in view TO DO: constrains between label and view

--- RGBSliders/ViewController.swift
+++ RGBSliders/ViewController.swift
@@ -8,14 +8,32 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet weak var mainView: UIView!
     
-   
+    
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    
+    @IBOutlet weak var redLabel: UILabel!
+    @IBOutlet weak var blueLabel: UILabel!
+    @IBOutlet weak var greenLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
+    @IBAction func changeColor() {
+        mainView.backgroundColor = UIColor(
+            red: CGFloat(redSlider.value),
+            green: CGFloat(greenSlider.value),
+            blue: CGFloat(blueSlider.value),
+            alpha: 1
+        )
+        redLabel.text = String(format: "%.2f", (redSlider.value))
+    }
+    
    
     
 }
