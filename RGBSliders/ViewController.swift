37332b07e5bfa83d0a6c4352dfbaef344acd1561
David <ber.dev@yandex.ru>
2021-03-27 20:07:24 +0300

    Constrains for sliders and labels

--- RGBSliders/ViewController.swift
+++ RGBSliders/ViewController.swift
@@ -9,11 +9,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mainView: UIView!
+    
+   
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
-
+   
+    
 }
 
